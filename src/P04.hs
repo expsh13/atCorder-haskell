42e1b7e8a7ab720ce2e2340d995bb8f931f5ae02
Satoki Hata <satokihata@gmail.com>
2025-12-17 16:26:58 +0900

    setup

---
+++ src/P04.hs
@@ -0,0 +1,6 @@
+module P04 where
+
+main :: IO ()
+main = do
+  -- TODO: 問題の入出力処理を実装
+  putStrLn "P04"
---
+++ src/P05.hs
@@ -0,0 +1,6 @@
+module P05 where
+
+main :: IO ()
+main = do
+  -- TODO: 問題の入出力処理を実装
+  putStrLn "P05"
