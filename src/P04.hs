b73bb1bfec3cb11f211170f6faefb45aa16709e0
Satoki Hata <satokihata@gmail.com>
2025-12-18 15:11:46 +0900

    04

--- src/P04.hs
+++ src/P04.hs
@@ -1,6 +1,21 @@
 module P04 where
 
+data Result = OK | NG deriving (Show)
+
 main :: IO ()
 main = do
-  -- TODO: 問題の入出力処理を実装
-  putStrLn "P04"
+  k <- read <$> getLine
+  [a, b] <- map (read :: String -> Int) . words <$> getLine
+
+  let firstMultiple = ((a + k - 1) `div` k) * k
+  let result = if firstMultiple <= b then OK else NG
+  print result
+
+-- O(B-A) になる。
+-- checkKTimes :: (Int, Int, Int) -> Result
+-- checkKTimes (k, a, b)
+--   | b `mod` k == 0 = OK
+--   | nextB < a = NG
+--   | otherwise = checkKTimes (k, a, nextB)
+--   where
+--     nextB = b - 1
