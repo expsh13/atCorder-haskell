dc9637dcc1ff37a8b2afac59581a7dd32912c9a1
Satoki Hata <satokihata@gmail.com>
2025-12-21 22:30:44 +0900

    05

--- src/P05.hs
+++ src/P05.hs
@@ -2,5 +2,33 @@ module P05 where
 
 main :: IO ()
 main = do
-  -- TODO: 問題の入出力処理を実装
-  putStrLn "P05"
+  [n, a, b] <- map (read :: String -> Int) . words <$> getLine
+  -- a,bは36以下の整数。
+
+  -- 1. 格桁のMaxの数は上記なのでaの最少の値を確認し、何桁から確認する必要があるか決める。
+  -- 2. 1で決まった値からNまでの値で制約を満たす整数を調べる
+  let minNum = (10 :: Int) ^ (minDigit a - 1)
+  let result = passedSum a b n minNum 0
+  print result
+
+-- 1桁→9
+-- 2桁→18
+-- 3桁→27
+-- 4桁→36
+minDigit :: Int -> Int
+minDigit a
+  | a > 27 = 4
+  | a > 18 = 3
+  | a > 9 = 2
+  | otherwise = 1
+
+-- 格桁の総和
+digitSum :: Int -> Int
+digitSum n = sum $ map (read . return) (show n)
+
+passedSum :: Int -> Int -> Int -> Int -> Int -> Int
+passedSum a b n curNum total
+  | curNum > n = total
+  | digitSum curNum >= a && digitSum curNum <= b =
+      passedSum a b n (curNum + 1) (total + curNum)
+  | otherwise = passedSum a b n (curNum + 1) total
