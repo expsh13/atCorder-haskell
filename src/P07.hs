60ce5ad200210967ba866b73a1d29b22a290c823
Satoki Hata <satokihata@gmail.com>
2025-12-22 14:23:06 +0900

    07

--- src/P07.hs
+++ src/P07.hs
@@ -1,6 +1,25 @@
 module P07 where
 
+import Data.List (sort)
+
 main :: IO ()
 main = do
-  -- TODO: 問題の入出力処理を実装
-  putStrLn "P07"
+  _ <- getLine
+  array <- map (read :: String -> Int) . words <$> getLine
+  let sorted = reverse . sort $ array
+  let (a, b) = splitNum sorted [] []
+  let result = diff a b
+  print result
+
+splitNum :: [Int] -> [Int] -> [Int] -> ([Int], [Int])
+splitNum [] a b = (a, b)
+splitNum (x : xs) a b =
+  if length a == length b
+    then splitNum xs (x : a) b
+    else splitNum xs a (x : b)
+
+diff :: [Int] -> [Int] -> Int
+diff a b = alice - bob
+  where
+    alice = sum a
+    bob = sum b
