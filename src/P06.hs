baffeb895723868363d8c60c3782cd493c8adf73
Satoki Hata <satokihata@gmail.com>
2025-12-22 13:48:48 +0900

    06

--- src/P06.hs
+++ src/P06.hs
@@ -1,6 +1,24 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module P06 where
 
 main :: IO ()
 main = do
-  -- TODO: 問題の入出力処理を実装
-  putStrLn "P06"
+  n <- getLine
+  putStrLn "配列入力"
+  array <- map (read :: String -> Int) . words <$> getLine
+  let result = foldl min 19683 (map evenCount array)
+  print result
+
+evenCount :: Int -> Int
+evenCount n = evenCountHelper n 0
+  where
+    evenCountHelper n c =
+      if even n
+        then evenCountHelper (n `div` 2) (c + 1)
+        else c
+
+logBase2 :: Double -> Int
+logBase2 n = floor log2
+  where
+    log2 = logBase 2 n
