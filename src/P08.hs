9eaa785a177766a3f9d713eb9c777361549176db
Satoki Hata <satokihata@gmail.com>
2025-12-22 15:21:04 +0900

    08

--- src/P08.hs
+++ src/P08.hs
@@ -1,6 +1,33 @@
 module P08 where
 
+import Data.Char (isLower)
+
+data Result = AC | WA deriving (Show)
+
 main :: IO ()
 main = do
-  -- TODO: 問題の入出力処理を実装
-  putStrLn "P08"
+  s <- getLine
+  let result = check s
+  print result
+
+isStartA :: String -> Bool
+isStartA str = head str == 'A'
+
+isInCludeC :: String -> Bool
+isInCludeC str = (== 1) $ length $ filter (== 'C') filterStr
+  where
+    filterStr = init $ tail $ tail str
+
+isLowerCase :: String -> Bool
+isLowerCase str = all isLower nonACChars
+  where
+    tailStr = tail str -- 先頭のA以外
+    withoutLast = init tailStr -- 2文字目〜末尾-1文字目
+    lastCh = last tailStr -- 末尾
+    nonACChars = filter (/= 'C') withoutLast ++ [lastCh]
+
+check :: String -> Result
+check str =
+  if isStartA str && isInCludeC str && isLowerCase str
+    then AC
+    else WA
