f7acdd851d32fafc9aa14cd00a5d1b23f3732f58
Satoki Hata <satokihata@gmail.com>
2025-12-24 22:42:08 +0900

    12

--- src/P12.hs
+++ src/P12.hs
@@ -1,5 +1,20 @@
 module P12 where
 
+import Control.Monad (replicateM)
+import qualified Data.Map as Map
+
 main :: IO ()
 main = do
-  putStrLn "P12"
+  n <- getInt
+  d <- replicateM n getInt
+  let result = Map.size $ createMap d Map.empty
+  print result
+
+getInt :: IO Int
+getInt = (read :: String -> Int) <$> getLine
+
+createMap :: [Int] -> Map.Map Int Int -> Map.Map Int Int
+createMap [] mapItem = mapItem
+createMap (x : xs) mapItem = case Map.lookup x mapItem of
+  Just _ -> createMap xs mapItem
+  Nothing -> createMap xs $ Map.insert x 1 mapItem
