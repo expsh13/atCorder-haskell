cc3cdb2e33a603003dc69d2acc2d5f8813859f42
Satoki Hata <satokihata@gmail.com>
2025-12-24 21:15:40 +0900

    11

--- src/P11.hs
+++ src/P11.hs
@@ -2,4 +2,14 @@ module P11 where
 
 main :: IO ()
 main = do
-  putStrLn "P11"
+  [n, y] <- map (read :: String -> Int) . words <$> getLine
+  let (a, b, c) = checkPattern n y
+  putStrLn $ show a ++ " " ++ show b ++ " " ++ show c
+
+checkPattern :: Int -> Int -> (Int, Int, Int)
+checkPattern n y =
+  if null pattern
+    then (-1, -1, -1)
+    else head pattern
+  where
+    pattern = [(a, b, c) | a <- [0 .. n], b <- [0 .. n - a], let c = n - a - b, c >= 0, y == 10000 * a + 5000 * b + 1000 * c]
