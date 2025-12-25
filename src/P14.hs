f7c2c6d7d13fdadb4150c1881fb3a19f4eee1e25
Satoki Hata <satokihata@gmail.com>
2025-12-25 18:43:52 +0900

    14

--- src/P14.hs
+++ src/P14.hs
@@ -1,5 +1,21 @@
 module P14 where
 
+import Data.Char (isUpper, toLower)
+import Data.List (sortOn)
+
 main :: IO ()
 main = do
-  putStrLn "P14"
+  s <- getLine
+  let result = concat $ sortOn (map toLower) $ splitStr s []
+  putStrLn result
+
+-- 単語に分割
+splitStr :: String -> [String] -> [String]
+splitStr "" array = array
+splitStr (x : xs) array = splitStr remain $ word : array
+  where
+    (upperBefore, upperAfter) = break isUpper xs
+    (word, remain) =
+      if null upperAfter
+        then (x : upperBefore, "")
+        else (x : upperBefore ++ [head upperAfter], tail upperAfter)
