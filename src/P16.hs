b308d1e7749fc30d0eb6eeb49e2b5dcf030368b6
Satoki Hata <satokihata@gmail.com>
2026-01-11 15:23:21 +0900

    17

--- src/P16.hs
+++ src/P16.hs
@@ -1,5 +1,28 @@
 module P16 where
 
+import Control.Monad (replicateM)
+import Data.List.Split (splitOn)
+
 main :: IO ()
 main = do
-  putStrLn "P16"
+  [_, q] <- getInts
+  s <- getLine
+  querys <- replicateM q getInts
+  let result = unlines $ map show $ loopQuery s querys
+  putStrLn result
+
+getInts :: IO [Int]
+getInts = map (read :: String -> Int) . words <$> getLine
+
+countAC :: String -> Int
+countAC str = length (splitOn "AC" str) - 1
+
+loopQuery :: String -> [[Int]] -> [Int]
+loopQuery str = map (runQuery str)
+
+runQuery :: String -> [Int] -> Int
+runQuery str query = countAC s
+  where
+    l = head query
+    r = head $ tail query
+    s = drop (l - 1) (take r str)
--- src/P17.hs
+++ src/P17.hs
@@ -1,5 +1,16 @@
 module P17 where
 
+import Control.Monad (guard)
+import GHC.Natural (Natural)
+
 main :: IO ()
 main = do
   putStrLn "P17"
+
+divisorsOfOdds :: [Natural] -> [Natural]
+divisorsOfOdds ints = do
+  i <- ints
+  guard $ i `rem` 2 == 1
+  j <- [1 .. i]
+  guard $ i `rem` j == 0
+  pure j
